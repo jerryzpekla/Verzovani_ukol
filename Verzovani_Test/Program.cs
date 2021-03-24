49ca82435bad4ecb4937d15dc55d2a6e93cb4bc4
jerryzpekla <jaroslav.tesar13seznam.cz>
2021-03-24 12:46:28 +0100

    zmena

---
+++ Verzovani_Test/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jaros
+ * Date: 24.03.2021
+ * Time: 12:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Verzovani_Test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			int counter = 0;
+			for (int i = 0; i < 15; i++) {  //Pocita do 15
+				counter ++;
+			}
+			Console.WriteLine(counter);
+			
+			Console.Write("tenhle vypis je zmena");
+			
+			
+			Console.ReadKey(true);
+		}
+	}
+}
